e1687addd195e0e6007d9023c23b73c85279dd0b
ajimmyyy <80930376+ajimmyyy@users.noreply.github.com>
2023-12-26 13:54:21 +0800

    初始化

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SelectFrequentItems',
+    version='0.1',
+    author='jimmy',
+    description='Select frequent item sets',
+    packages = find_packages(),
+    install_requires=[
+        "pandas",
+        'numpy',
+        "mlxtend",
+        "matplotlib"
+    ],
+)
